e670a43424a3f543dbbcb53a477ae866cbcaef5e
tanzilanaaz <113665972+tanzilanaaz@users.noreply.github.com>
2024-10-01 17:04:06 +0530

    Add files via upload

--- views.py
+++ views.py
@@ -1,11 +1,4 @@
 from django.shortcuts import render
 
-
-def home(request):
-    return render(request, "home.html")
-
-def contact_us(request):
-    return render(request, "contactus.html")
-
-def about_us(request):
-    return render(request, "aboutus.html")
+def fun(request):
+    return render(request,"astiApp/test.html")
