34459d178ff8f129fea3f1a86473810ac1b36160
tanzilanaaz <113665972+tanzilanaaz@users.noreply.github.com>
2024-10-01 17:06:08 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,30 +1,12 @@
-"""asti_project URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('home/', views.home),
-    path('astiApp/', include("astiApp.urls")),
-    path('contactus/', views.contact_us),
-    path('aboutus/', views.about_us),
-    path('student/', include('studentapp.urls')),
-    path('employee/', include('empapp.urls')),
-    path('course/', include('course.urls')),
-    path('api/', include('empapi.urls')),
+    path('ems/', views.ems),
+    path('g-emp/<pk>/', views.get_employee_by_id, name='get_employee'),
+    path('u-emp/<pk>/', views.update_employee_by_id, name='update_employee'),
+    path('d-emp/<pk>/', views.delete_employee_by_id, name='delete_employee'),
 ]
+
+# http://localhost:8000/employee/ems/ -----> url to access emp related things
--- views.py
+++ views.py
@@ -1,11 +1,68 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Employee
 
 
-def home(request):
-    return render(request, "home.html")
+def ems(request):
+    if request.method == 'GET':
+        # 1. get all details from database
+        emps_db = Employee.objects.all()
 
-def contact_us(request):
-    return render(request, "contactus.html")
+        # 2. construct a context object
+        emp_context = {
+            "employees": emps_db,
+        }
+        return render(request, 'empapp/ems.html', context=emp_context)
+    elif request.method == 'POST':
+        #1. read the employee input data
+        emp_empid = request.POST.get('emp_empid')
+        name = request.POST.get('emp_name')
+        age = request.POST.get('emp_age')
+        salary = request.POST.get('emp_salary')
+        address = request.POST.get('emp_address')
+        doj = request.POST.get('emp_doj')
+        designation = request.POST.get('emp_designation')
 
-def about_us(request):
-    return render(request, "aboutus.html")
+        #2. create a employee object using above input data
+        employee = Employee(empid=emp_empid, name=name, age=age, salary=salary,
+                            address=address, doj=doj, designation=designation)
+
+        #3. call save method of student model object to insert student data into the database
+        employee.save()
+
+        #4. return success response to the client
+        return redirect('/employee/ems/')
+
+
+def get_employee_by_id(request, pk):
+    return redirect('/employee/ems/')
+
+
+def update_employee_by_id(request, pk):
+    #step1: get details from db based on pk
+    employee_db = Employee.objects.get(id=pk)
+
+    #step2: update the details based on input values
+    employee_db.empid = request.POST.get("emp_empid")
+    employee_db.name = request.POST.get("emp_name")
+    employee_db.age = request.POST.get("emp_age")
+    employee_db.salary = request.POST.get("emp_salary")
+    employee_db.address = request.POST.get("emp_address")
+    employee_db.doj = request.POST.get("emp_doj")
+    employee_db.designation = request.POST.get("emp_designation")
+
+    #step3: call save method to update latest values
+    employee_db.save()
+
+    #step4: redirect to ems page
+    return redirect('/employee/ems/')
+
+
+def delete_employee_by_id(request, pk):
+    # step1: get details from db based on pk
+    employee_db = Employee.objects.get(id=pk)
+
+    # step2: call delete method  on employee object
+    employee_db.delete()
+
+    # step3 : redirect request to employee ems page
+    return redirect('/employee/ems/')
